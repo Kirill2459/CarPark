3558ec02da7a8d5e3cd3210d8db655a89802a9ac
Kirill <eon259@mail.ru>
2025-09-12 22:35:19 +0700

    Добавлена главная форма для сущности Car

--- WindowsFormsApp/Form1.Designer.cs
+++ WindowsFormsApp/Form1.Designer.cs
@@ -28,13 +28,312 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView_Cars = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView_Owners = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Cars)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Owners)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1199, 596);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button7);
+            this.tabPage1.Controls.Add(this.button6);
+            this.tabPage1.Controls.Add(this.groupBox4);
+            this.tabPage1.Controls.Add(this.groupBox3);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Controls.Add(this.dataGridView_Cars);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1191, 567);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Cars";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(543, 498);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(368, 33);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "Добавить автомобиль в автопарк";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(7, 498);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(529, 33);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "Узнать полную стоимость автопарка";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.button5);
+            this.groupBox4.Controls.Add(this.textBox3);
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Location = new System.Drawing.Point(543, 376);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(368, 116);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Обновление информации об автомобиле";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(9, 76);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(353, 28);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Обновить информацию об автомобиле";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 48);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(135, 22);
+            this.textBox3.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(295, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Id автомобиля для обновления информации";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Location = new System.Drawing.Point(542, 270);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(369, 99);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Удаление автомобиля";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(9, 58);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(354, 34);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Удалить автомобиль";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(233, 30);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(130, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(6, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Id автомобиля для удаления";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(7, 270);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(529, 222);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Просмотр автомобилей";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(24, 118);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(474, 98);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Сортировка по бренду автомобиля";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(9, 55);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(459, 37);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Сортировать по бренду";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(247, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(221, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(6, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Укажите автомобильный бренд:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(24, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(474, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Найти старые машины";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(474, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Показать все машины";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView_Cars
+            // 
+            this.dataGridView_Cars.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Cars.Location = new System.Drawing.Point(7, 6);
+            this.dataGridView_Cars.Name = "dataGridView_Cars";
+            this.dataGridView_Cars.RowHeadersWidth = 51;
+            this.dataGridView_Cars.RowTemplate.Height = 24;
+            this.dataGridView_Cars.Size = new System.Drawing.Size(845, 245);
+            this.dataGridView_Cars.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView_Owners);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1191, 567);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Owners";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView_Owners
+            // 
+            this.dataGridView_Owners.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Owners.Location = new System.Drawing.Point(7, 64);
+            this.dataGridView_Owners.Name = "dataGridView_Owners";
+            this.dataGridView_Owners.RowHeadersWidth = 51;
+            this.dataGridView_Owners.RowTemplate.Height = 24;
+            this.dataGridView_Owners.Size = new System.Drawing.Size(635, 150);
+            this.dataGridView_Owners.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1200, 560);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "CarPark";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Cars)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Owners)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView_Cars;
+        private System.Windows.Forms.DataGridView dataGridView_Owners;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
     }
 }
 
--- WindowsFormsApp/Form1.cs
+++ WindowsFormsApp/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Model.Entities;
+using Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,99 @@ namespace WindowsFormsApp
 {
     public partial class Form1 : Form
     {
+        public List<Car> cars { get; set; } = new List<Car>();
+        public List<Owner> owners { get; set; } = new List<Owner>();
+        public List<Car> carsOwnedByOwners { get; set; } = new List<Car>(); //машины принадлежащие владельцам
+
+
+
+
         public Form1()
         {
             InitializeComponent();
+            owners = Logic.ReadAll<Owner>();
+            dataGridView_Owners.DataSource = owners;
+        }
+
+        //показать все машины
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cars = Logic.ReadAll<Car>();
+            dataGridView_Cars.DataSource = cars;
+        }
+
+        //найти старые машины
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cars = Logic.GetVintageCars();
+            dataGridView_Cars.DataSource = cars;
+        }
+
+        //сортировать по бренду
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string brand = textBox1.Text;
+            cars = Logic.GetCarsByBrand(brand);
+            dataGridView_Cars.DataSource = cars;
+        }
+
+        //удалить автомобиль
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int id = int.Parse(textBox2.Text);
+
+                List<Owner> owners = Logic.ReadAll<Owner>();
+                Car car = Logic.Read<Car>(id);
+
+                if (car != null)
+                {
+                    Logic.Delete<Car>(id);
+                    foreach (Owner owner in owners)
+                    {
+                        // Проверяем содержит ли владелец эту машину
+                        if (owner.IdCarsOwner.Contains(id))
+                        {
+                            // Удаляем ID машины из списка владельца
+                            owner.IdCarsOwner.Remove(id);
+                            Logic.Update(owner); // Обновляем владельца в json
+
+                            MessageBox.Show($"Машина ID:{id} удалена у владельца {owner.Name}");
+                        }
+                    }
+                    MessageBox.Show($"Автомобиль удален.");
+                }
+                else
+                {
+                    MessageBox.Show("В автопарке нет автомобиля с таким Id.");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка.");
+            }
+            cars = Logic.ReadAll<Car>();
+            dataGridView_Cars.DataSource = cars;
+        }
+
+        //узнать полную стоимость автопарка
+        private void button6_Click(object sender, EventArgs e)
+        {
+            decimal allPrice = Logic.AllPrice();
+            MessageBox.Show($"Полная стоимость автопарка составляет {allPrice} рублей.");
+        }
+
+        //обновить информацию об автомобиле
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //....
+        }
+
+        //добавить автомобиль в автопарк
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //...
         }
     }
 }
